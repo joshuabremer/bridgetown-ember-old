374abb1cc2bcc48b2547704f893ed0a8372bc24e
Josh Bremer <joshua.bremer@gmail.com>
2013-12-28 19:32:53 +0000

    Upload: scripts/schedule.js

---
+++ scripts/schedule.js
@@ -0,0 +1,13 @@
+(function() {
+  var EventModel = Backbone.View.Model({
+
+  });
+
+  var EventCollection = Backbone.View.Collection({
+
+  });
+
+  var ScheduleView = Backbone.View.extend({
+
+  });
+}())
