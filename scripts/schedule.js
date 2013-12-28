013e3f0f06d0d9f1b93334a15410ef72aa61585e
Josh Bremer <joshua.bremer@gmail.com>
2013-12-28 19:47:29 +0000

    Upload: scripts/schedule.js

--- scripts/schedule.js
+++ scripts/schedule.js
@@ -5,7 +5,23 @@
 
   var EventCollection = Backbone.Collection.extend({
     model: Event,
-    url: 'https://bridgetown-dev.squarespace.com/events/?format=json'
+    url: 'https://bridgetown-dev.squarespace.com/events/?format=json',
+
+    // Modified from default fetch function
+    fetch: function(options) {
+      options = options ? _.clone(options) : {};
+      if (options.parse === void 0) options.parse = true;
+      var success = options.success;
+      var collection = this;
+      options.success = function(resp) {
+        var method = options.reset ? 'reset' : 'set';
+        collection[method](resp, options);
+        if (success) success(collection, resp, options);
+        collection.trigger('sync', collection, resp, options);
+      };
+      wrapError(this, options);
+      return this.sync('read', this, options);
+    }
 
   });
 
