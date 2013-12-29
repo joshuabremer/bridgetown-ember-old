2ad769093cdf461868ff827b0ef4f315eb3e45d1
Josh Bremer <joshua.bremer@gmail.com>
2013-12-29 20:28:24 +0000

    Upload: scripts/main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,26 +1,7 @@
 require.config({
   paths : {
-    underscore : 'lib_underscore',
-    backbone   : 'lib_backbone',
-    marionette : 'lib_backbone.marionette',
-    jquery     : 'lib_jquery',
-    tpl        : 'lib_tpl'
-  },
-  shim : {
-    'lib_backbone-localStorage' : ['backbone'],
-    underscore : {
-      exports : '_'
-    },
-    backbone : {
-      exports : 'Backbone',
-      deps : ['jquery','underscore']
-    },
-    marionette : {
-      exports : 'Backbone.Marionette',
-      deps : ['backbone']
-    }
-  },
-  deps : ['jquery','underscore']
+    underscore : 'lib_underscore'
+  }
 });
 
 // require(['app','backbone','routers/index','controllers/index'],function(app,Backbone,Router,Controller){
