d7d98243e7b4b98ebbbcd86fee27f446254ea65b
Josh Bremer <joshua.bremer@gmail.com>
2013-12-29 20:23:38 +0000

    Upload: scripts/main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,27 +1,27 @@
-require.config({
-  paths : {
-    underscore : 'lib_underscore',
-    backbone   : 'lib_backbone',
-    marionette : 'lib_backbone.marionette',
-    jquery     : 'lib_jquery',
-    tpl        : 'lib_tpl'
-  },
-  shim : {
-    'lib_backbone-localStorage' : ['backbone'],
-    underscore : {
-      exports : '_'
-    },
-    backbone : {
-      exports : 'Backbone',
-      deps : ['jquery','underscore']
-    },
-    marionette : {
-      exports : 'Backbone.Marionette',
-      deps : ['backbone']
-    }
-  },
-  deps : ['jquery','underscore']
-});
+// require.config({
+//   paths : {
+//     underscore : 'lib_underscore',
+//     backbone   : 'lib_backbone',
+//     marionette : 'lib_backbone.marionette',
+//     jquery     : 'lib_jquery',
+//     tpl        : 'lib_tpl'
+//   },
+//   shim : {
+//     'lib_backbone-localStorage' : ['backbone'],
+//     underscore : {
+//       exports : '_'
+//     },
+//     backbone : {
+//       exports : 'Backbone',
+//       deps : ['jquery','underscore']
+//     },
+//     marionette : {
+//       exports : 'Backbone.Marionette',
+//       deps : ['backbone']
+//     }
+//   },
+//   deps : ['jquery','underscore']
+// });
 
 // require(['app','backbone','routers/index','controllers/index'],function(app,Backbone,Router,Controller){
 //   "use strict";
