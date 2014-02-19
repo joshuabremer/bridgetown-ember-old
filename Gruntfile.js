6b6d1c3f8597a34667f3051f45e8548ecedadca8
Josh Bremer <jbremer@sweetspotdiabetes.com>
2014-02-19 10:31:04 -0800

    Minified scripts

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,21 +6,59 @@ module.exports = function(grunt) {
     uglify: {
       options: {
         banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n',
-        mangle: false,
-        beautify: true,
+        //mangle: false,
+        //beautify: true,
         drop_debugger: false,
         drop_console: false
       },
       build: {
         //cwd: 'js',
-        src: ['js/models/*.js','js/views/*.js'],
-        dest: 'js/build.views.js'
+        src: [
+          'scripts/vendor_moment.js',
+          'scripts/vendor_spin.js',
+          'scripts/vendor_underscore-min.js',
+          //'scripts/vendor_jquery.js',
+          'scripts/vendor_bootstrap.js',
+          'scripts/vendor_handlebars.js',
+          // 'scripts/vendor_ember.js',
+          // 'scripts/vendor_ember-data.js',
+          'scripts/helpers_moment_helper.js',
+          'scripts/helpers_excerpt_helper.js',
+          'scripts/config_adapter.js',
+          'scripts/config_app.js',
+          'scripts/components_spin_component.js',
+          'scripts/models_category.js',
+          'scripts/models_event.js',
+          'scripts/models_newspost.js',
+          'scripts/models_performer.js',
+
+          'scripts/controllers_newspost_controller.js',
+          'scripts/controllers_performer_controller.js',
+          'scripts/controllers_catchall_controller.js',
+
+          'scripts/views_index_view.js',
+          'scripts/views_newspost_view.js',
+          'scripts/views_newsposts_view.js',
+          'scripts/views_header_view.js',
+
+          'scripts/templates.js',
+
+          'scripts/config_routes.js',
+
+          'scripts/routes_index_route.js',
+          'scripts/routes_newsposts_route.js',
+          'scripts/routes_newspost_route.js',
+          'scripts/routes_performers_route.js',
+          'scripts/routes_performer_route.js',
+          'scripts/routes_catchall_route.js'
+        ],
+        dest: 'scripts/application.js'
       }
     },
     watch: {
       scripts: {
-        files: ['**/*.js', 'index.html','**/*.scss'],
-        tasks: ['jshint','sass'],
+        files: ['Gruntfile.js','scripts/vendor.js', 'index.html','**/*.scss'],
+        tasks: ['jshint','uglify','sass'],
         options: {
           spawn: false,
           livereload: 1337
@@ -47,7 +85,7 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-sass');
 
-  grunt.registerTask('default', ['jshint','sass']);
+  grunt.registerTask('default', ['jshint','uglify','sass']);
 
 
 };
