dfaf458bebbcbff003aef2de79597053c0c40957
Josh Bremer <jbremer@sweetspotdiabetes.com>
2014-02-28 13:22:15 -0800

    Big changes

--- scripts/routes_index_route.js
+++ scripts/routes_index_route.js
@@ -1,12 +1,12 @@
 App.IndexRoute = Ember.Route.extend({
 
   model: function(params) {
-    return $.ajax({
-      url: 'http://bridgetown-dev.squarespace.com/newsposts/?format=json-pretty',
-      dataType:'jsonp'
-    }).done(function(data) {
-      return data.items;
-    });
+    // return $.ajax({
+    //   url: 'http://bridgetown-dev.squarespace.com/newsposts/?format=json-pretty',
+    //   dataType:'jsonp'
+    // }).done(function(data) {
+    //   return data.items;
+    // });
   },
 
   renderTemplate: function() {
