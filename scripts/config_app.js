c416aa5406e62ba183ff1f6fc0ccb224653829d1
Josh Bremer <jbremer@sweetspotdiabetes.com>
2014-03-02 23:24:31 -0800

    Changes

--- scripts/config_app.js
+++ scripts/config_app.js
@@ -3,20 +3,6 @@ var App = Ember.Application.create({
   rootElement: '#ember-app'
 });
 
-App.FixtureAdapter = DS.FixtureAdapter.extend({
-  queryFixtures: function(records, query, type) {
-    return records.filter(function(record) {
-        for(var key in query) {
-            if (!query.hasOwnProperty(key)) { continue; }
-            var value = query[key];
-            console.log(record[key]);
-            if (record[key] !== value) { return false; }
-        }
-        return true;
-    });
-  }
-});
-
 App.Store = DS.Store.extend({
   revision: 11,
   //adapter: DS.RESTAdapter.create()
