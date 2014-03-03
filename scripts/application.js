649cb107e2e266cbf62cd58d19576d9d165bc232
Josh Bremer <jbremer@sweetspotdiabetes.com>
2014-03-02 23:05:08 -0800

    Changes

--- scripts/application.js
+++ scripts/application.js
@@ -4502,7 +4502,7 @@ App.PerformerController = Ember.ObjectController.extend({
     this.resource("events"), this.resource("events", {
         path: "/events/:events_id"
     }), this.resource("performers"), this.resource("performer", {
-        path: "/performer/:Name"
+        path: "/performer/:id"
     }), this.route("venues"), this.route("shows"), this.route("history"), this.route("press"), 
     this.route("contact"), this.route("catch_all", {
         path: "*:"
