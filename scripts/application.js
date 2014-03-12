4e90de0448d203fd495489512a86e1465c139ba1
Josh Bremer <jbremer@sweetspotdiabetes.com>
2014-03-12 08:05:50 -0700

    Changes

--- scripts/application.js
+++ scripts/application.js
@@ -4105,10 +4105,7 @@ App.NewspostController = Ember.ObjectController.extend({}), App.NewspostsControl
 App.PerformerController = Ember.ObjectController.extend({
     sortProperties: [ "Tier" ],
     sortAscending: !0
-}), App.PerformersController = Ember.ArrayController.extend({
-    sortProperties: [ "Tier" ],
-    sortAscending: !0
-}), App.ApplicationView = Ember.View.extend({
+}), App.PerformersController = Ember.ArrayController.extend({}), App.ApplicationView = Ember.View.extend({
     didInsertElement: function() {
         var a, b = document.getElementsByTagName("script")[0], c = /^http:/.test(document.location) ? "http" : "https";
         document.getElementById("twitter-wjs") || (a = document.createElement("script"), 
--- scripts/controllers_performers_controller.js
+++ scripts/controllers_performers_controller.js
@@ -1,5 +1,4 @@
 App.PerformersController = Ember.ArrayController.extend({
-  sortProperties: ['Tier'],
-  sortAscending: true
+
 });
 
