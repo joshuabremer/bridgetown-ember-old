30533d5086beed742e163194d212ba06b87845fd
Joshua Bremer <joshua.bremer@gmail.com>
2014-04-02 22:07:53 -0700

    Got to figure out how to do rows

---
+++ scripts/fixtures_venue.js
@@ -0,0 +1,26 @@
+/*jshint -W100 */
+App.Venue.FIXTURES = ﻿[
+  {
+    "id":1,
+    "Name": "Hawthorne",
+    "events":[1]
+  },
+
+  {
+    "id":2,
+    "Name": "Bossanova",
+    "events":[2]
+  },
+
+  {
+    "id":3,
+    "Name": "Hollywood",
+    "events":[3]
+  },
+
+  {
+    "id":4,
+    "Name": "Mt. Tabor" ,
+    "events":[4]
+  }
+];
