82d8f68200bda698b15d43afb20c927893ba7583
Joshua Bremer <joshua.bremer@gmail.com>
2014-04-06 22:47:16 -0700

    merge

--- scripts/fixtures_venue.js
+++ scripts/fixtures_venue.js
@@ -1,6 +1,7 @@
 /*jshint -W100 */
 App.Venue.FIXTURES = ﻿[
   {
+<<<<<<< Updated upstream
     "id":1,
     "Name": "Hawthorne",
     "events":[1]
@@ -24,3 +25,57 @@ App.Venue.FIXTURES = ﻿[
     "events":[4]
   }
 ];
+=======
+  Name: "Alhambra Theater Lounge",
+  id: "7608"
+  },
+  {
+  Name: "Alhambra Theater Main",
+  id: "7609"
+  },
+  {
+  Name: "Analog Lounge",
+  id: "7620"
+  },
+  {
+  Name: "Analog Theater",
+  id: "7619"
+  },
+  {
+  Name: "Bar of the Gods",
+  id: "7610"
+  },
+  {
+  Name: "Bossanova",
+  id: "7611"
+  },
+  {
+  Name: "Doug Fir",
+  id: "7612"
+  },
+  {
+  Name: "Eagles Lodge",
+  id: "7613"
+  },
+  {
+  Name: "Hawthorne Theatre Lounge",
+  id: "7614"
+  },
+  {
+  Name: "Hawthorne Theatre Main",
+  id: "7615"
+  },
+  {
+  Name: "Jupiter Tent",
+  id: "7616"
+  },
+  {
+  Name: "The Tanker",
+  id: "7617"
+  },
+  {
+  Name: "White Owl Social Club",
+  id: "7618"
+  }
+];
+>>>>>>> Stashed changes
--- scripts/models_venue.js
+++ scripts/models_venue.js
@@ -1,7 +1,14 @@
 App.Venue = DS.Model.extend({
 
+<<<<<<< Updated upstream
   events: DS.hasMany('event', {async: true}),
 
   Name: DS.attr('string')
 
 });
+=======
+  Name: DS.attr('string')
+
+});
+
+>>>>>>> Stashed changes
