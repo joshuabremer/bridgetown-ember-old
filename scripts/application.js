7accf7418ab0cd0844d8e3cd87c92d868c672581
Joshua Bremer <joshua.bremer@gmail.com>
2014-05-11 09:47:20 -0700

    Updated shows route

--- scripts/application.js
+++ scripts/application.js
@@ -10913,13 +10913,14 @@ App.PerformerController = Ember.ObjectController.extend({}), App.PerformersContr
     title: "Show"
 }), App.ShowsRoute = Ember.Route.extend({
     model: function() {
+        var _this = this;
         return Ember.RSVP.hash({
             venues: this.store.find("venue"),
             events: this.store.find("event"),
             performers: this.store.find("performer"),
             shows: this.store.find("show")
         }).then(function() {
-            return this.store.find("show");
+            return _this.store.find("show");
         });
     },
     title: "Shows"
--- scripts/routes_shows_route.js
+++ scripts/routes_shows_route.js
@@ -1,12 +1,13 @@
 App.ShowsRoute = Ember.Route.extend({
   model: function(params) {
+    var _this = this;
     return Ember.RSVP.hash({
         venues: this.store.find('venue'),
         events: this.store.find('event'),
         performers: this.store.find('performer'),
         shows: this.store.find('show')
     }).then(function() {
-      return this.store.find('show');
+      return _this.store.find('show');
     });
   },
   title: 'Shows'
