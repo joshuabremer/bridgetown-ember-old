18b5cc29ac3eb86b5137723b65cd62098177db19
Josh Bremer <jbremer@sweetspotdiabetes.com>
2014-10-05 23:46:21 -0700

    Properly compiled

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,7 +13,7 @@ module.exports = function(grunt) {
       },
       build: {
         src: [
-          'vendor_responsive_tables.js',
+          'vendor/responsive_tables.js',
           'scripts/vendor/moment.js',
           'scripts/vendor/spin.js',
           'scripts/vendor/underscore-min.js',
